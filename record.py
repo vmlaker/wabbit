4d5b2e841840b42d162deb095e146d4cd516c16b
vmlaker <velimir.mlaker@gmail.com>
2013-06-16 16:40:39 -0700

    Change ticker, skip user input.

--- record.py
+++ record.py
@@ -6,7 +6,7 @@ import sys
 import cv2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from coils import time2fname, time2dir, user_input, Config, RateTicker
+import coils
 from Image import Image, Base
 
 DEVICE = int(sys.argv[1])
@@ -20,43 +20,43 @@ cap.set(3, WIDTH)
 cap.set(4, HEIGHT)
 
 # Create the display window.
-title = 'playing OpenCV capture'
+title = 'wabbit'
 cv2.namedWindow(title, cv2.cv.CV_WINDOW_NORMAL)
 
 # Load configuration file.
-config = Config('wabbit.cfg')
+config = coils.Config('wabbit.cfg')
 
 # Connect to database engine and start a session.
-dbname = user_input('Database', default=config['db_name'])
-user1 = user_input('Username', default=config['username'])
-pass1 = user_input('Password', default=config['password'])
-host = user_input('Host', default=config['host'])
-engine = create_engine('mysql://{}:{}@{}/{}'.format(user1, pass1, host, dbname))
+engine = create_engine(
+    'mysql://{}:{}@{}/{}'.format(
+        config['username'], config['password'], 
+        config['host'], config['db_name']))
 conn = engine.connect()
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # Monitor framerates for past few seconds.
-ticker = RateTicker((1, 2, 5))
+ticker = coils.RateTicker((1, 2, 5))
 
 # Go into main loop.
 end = datetime.now() + timedelta(seconds=DURATION)
 while end > datetime.now():
-
-    print('{}, {}, {}'.format(*ticker.tick()))
 
     # Take a snapshot and mark the timestamp.
     hello, image = cap.read()
     now = datetime.now()
 
     # Create destination directory.
-    dname = time2dir(now)
+    dname = coils.time2dir(now)
+    dname = os.path.join(config['pics_dir'], dname)
     if not os.path.isdir(dname):
         os.makedirs(dname)
 
     # Save the file.
-    fname = os.path.join(dname, time2fname(now)) + '.png'
+    timer = coils.Timer()
+    fname = os.path.join(dname, coils.time2fname(now)) + '.png'
     cv2.imwrite(fname, image)
+    t1 = timer.get().total_seconds()
 
     # Create the object.
     Base.metadata.create_all(engine)
@@ -67,14 +67,17 @@ while end > datetime.now():
 
     # Commit the transaction.
     session.commit()
+    t2 = timer.get().total_seconds()
 
     # Display the image.
     cv2.imshow(title, image)
     cv2.waitKey(1)
 
+    print('{}, {}, {}  {:.3f}  {:.3f}'.format(
+            *ticker.tick() + (t1, t2)))
+
 # Close the session.
 conn.close()
-
 
 
 
