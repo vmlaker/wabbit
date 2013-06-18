2d62b7d08de3c1ba03f36e69d59d6154583fc8ae
vmlaker <velimir.mlaker@gmail.com>
2013-06-18 11:33:24 -0700

    Use unordered stages, and add optional configuration file command-line parameter.

--- record.py
+++ record.py
@@ -9,22 +9,20 @@ from sqlalchemy.orm import sessionmaker
 import coils
 from Image import Image, Base
 
+# Read command-line parameters.
 DEVICE = int(sys.argv[1])
 WIDTH = int(sys.argv[2])
 HEIGHT = int(sys.argv[3])
 DURATION = float(sys.argv[4])
+CONFIG = sys.argv[5] if len(sys.argv)>=6 else 'wabbit.cfg'
 
 # Create the OpenCV video capture object.
 cap = cv2.VideoCapture(DEVICE)
 cap.set(3, WIDTH)
 cap.set(4, HEIGHT)
 
-# Create the display window.
-#title = 'wabbit'
-#cv2.namedWindow(title, cv2.cv.CV_WINDOW_NORMAL)
-
 # Load configuration file.
-config = coils.Config('wabbit.cfg')
+config = coils.Config(CONFIG)
 
 # Connect to database engine and start a session.
 engine = create_engine(
@@ -65,11 +63,10 @@ def write2db(tstamp):
     # Commit the transaction.
     session.commit()
 
-from mpipe import OrderedStage, Pipeline
-stage1 = OrderedStage(save2disk, 3)
-stage2 = OrderedStage(write2db, 3)
-stage1.link(stage2)
-pipe1 = Pipeline(stage1)
+from mpipe import Pipeline, UnorderedStage as UStage
+stage1 = UStage(save2disk, 3)
+stage2 = UStage(write2db, 3)
+pipe1 = Pipeline(stage1.link(stage2))
 
 # Go into main loop.
 end = datetime.now() + timedelta(seconds=DURATION)
@@ -82,16 +79,12 @@ while end > datetime.now():
     pipe1.put((now, image))
 
     # Display the image.
-    #cv2.imshow(title, image)
+    #cv2.namedWindow('wabbit', cv2.cv.CV_WINDOW_NORMAL)
+    #cv2.imshow('wabbit', image)
     #cv2.waitKey(1)
 
     print('{}, {}, {}'.format(*ticker.tick()))
 
-# Close the session.
+# Close the session and stop the pipeline.
 conn.close()
-
 pipe1.put(None)
-
-
-
-
