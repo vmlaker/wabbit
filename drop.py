b4e49668819e00b3f4284c5f737bdda9e4f784a0
vmlaker <velimir.mlaker@gmail.com>
2013-06-28 17:28:17 -0700

    Change to module imports.

--- drop.py
+++ drop.py
@@ -1,17 +1,17 @@
 """Drop MySQL user and database."""
 
 import sys
-from coils import user_input, Config
-from sqlalchemy import create_engine
+import sqlalchemy as sa
+import coils
 
 # Load configuration file.
 CONFIG = sys.argv[1] if len(sys.argv)>=2 else 'wabbit.cfg'
-config = Config(CONFIG)
+config = coils.Config(CONFIG)
 
 # Connect to database engine.
-root_u = user_input('Admin username', default=config['admin'])
-root_p = user_input('Admin password', password=True)
-engine = create_engine(
+root_u = coils.user_input('Admin username', default=config['admin'])
+root_p = coils.user_input('Admin password', password=True)
+engine = sa.create_engine(
         'mysql://{}:{}@{}'.format(root_u, root_p, config['host']))
 conn = engine.connect()
 
