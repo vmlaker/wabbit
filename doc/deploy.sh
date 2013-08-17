769d5b370146b42f750598bc1d18ea271559cef9
vmlaker <velimir.mlaker@gmail.com>
2013-08-16 17:17:35 -0700

    Add documentation.

---
+++ doc/deploy.sh
@@ -0,0 +1,10 @@
+# Sequence of commands used to deploy content to gh-pages branch.
+
+git checkout gh-pages
+rm -rf *
+git checkout master doc
+git reset HEAD
+mv doc/* .
+rm -rf doc
+git add .
+git commit -m "Updated gh-pages for `git log master -1 | head -1`"
