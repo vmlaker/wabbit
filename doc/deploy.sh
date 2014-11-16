1309ba910265e697d0061044fba1a3d8907b6748
vmlaker <velimir.mlaker@gmail.com>
2014-11-16 15:41:46 -0800

    Fix doc deploy script to properly remove the old, and add the new stuff.

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -10,7 +10,7 @@ scons bites=../bites -j 9 . doc
 git status
 
 git checkout gh-pages
-find . -maxdepth 1 ! -name '.git*' -delete
+find . -maxdepth 1 ! -name '.git*' | xargs rm -rf
 git checkout master doc
 git reset HEAD
 mv doc/* .
@@ -19,6 +19,7 @@ rm -rf deploy.sh forkme.xcf tile.xcf
 git add .
 
 # Sanity check before commit.
+# May have to run "git add --all ." 
 git status
 
 git commit -m "Updated gh-pages for `git log master -1 | head -1`"
