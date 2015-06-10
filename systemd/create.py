7e285e4c613c5aca2a965a47eb96534b5f9a4878
vmlaker <velimir.mlaker@gmail.com>
2015-06-10 13:53:39 -0700

    Name the service by db_name in config file.

--- systemd/create.py
+++ systemd/create.py
@@ -6,13 +6,20 @@ from os import chdir
 from os.path import dirname, join, normpath, realpath
 import glob
 import getpass
+import sys
+
+import coils
+
+# Load configuration file.
+CONFIG = sys.argv[1] if len(sys.argv)>=2 else 'wabbit.conf'
+config = coils.Config(CONFIG)
 
 # Do the work in the directory of this file.
 this_dir = dirname(realpath(__file__))
 chdir(this_dir)
 
-# Create the configuration dict.
-config = {
+# Add items to configuration.
+for key, val in {
     'WORKING_DIR' : normpath(join(this_dir, '..')),
     'RECORD_CMD'  : normpath(join(this_dir, '..', 'bin', 'record')),
     'GUNICORN_BIN': normpath(join(this_dir, '..', 'venv', 'bin', 'gunicorn')),
@@ -20,13 +27,14 @@ config = {
     'ADDRESS'     : '127.0.0.1:8000',
     'USER'        : getpass.getuser(),
     'GROUP'       : getpass.getuser(),
-}
+}.items():
+    config[key] = val
 
 # For each *.service.in template, create a real .service 
 # file with text replacement as per the configuration.
 for fname in glob.glob('*.service.in'):
     with open(fname) as inf:
-        outf = fname[:-3]
+        outf = config['db_name'] + '-' + fname[:-3]
         with open(outf, 'w') as outf:
             for line in inf.readlines():
                 for key, val in config.items():
