02f50cc01558a6151fcb84cd64ae826e12a6804d
vmlaker <velimir.mlaker@gmail.com>
2015-12-31 13:29:07 -0800

    Add timeouts to websocket handling to play nice.

--- src/py/websocket_serve.py
+++ src/py/websocket_serve.py
@@ -4,6 +4,8 @@ Serve webcam images from Memcached over a websocket.
 
 # Import standard modules.
 import base64
+from datetime import datetime
+from collections import defaultdict
 import os
 import sys
 
@@ -19,15 +21,28 @@ class IndexHandler(web.RequestHandler):
         self.render('websocket.html')
 
 class SocketHandler(websocket.WebSocketHandler):
+    clients = defaultdict(str)
+
     def __init__(self, *args, **kwargs):
         super(SocketHandler, self).__init__(*args, **kwargs)
         servers = ['127.0.0.1:11211']
         self._mc = Client(servers, debug=1)
 
+    def open(self):
+        pass
+
     def on_message(self, message):
-        value = self._mc.get('image')
-        image = base64.b64encode(value)
-        self.write_message(image)
+        time = self._mc.get('time')
+        if self.clients[self] == time:
+            self.write_message({'timeout': 50})
+        else:
+            image = self._mc.get('image')
+            image = base64.b64encode(image)
+            self.write_message({'image': image, 'timeout': 80})
+            self.clients[self] = time
+
+    def on_close(self):
+        del self.clients[self]
 
 template_folder = os.path.normpath(os.path.join(os.getcwd(), 'templates'))
 static_folder = os.path.normpath(os.path.join(os.getcwd(), 'static'))
